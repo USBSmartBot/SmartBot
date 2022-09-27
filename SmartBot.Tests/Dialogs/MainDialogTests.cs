ef74037d3bea9940883061df2d8a7f23624ff866
USBSmartBot <kirankj4@outlook.com>
2022-09-26 19:06:33 -0500

    commit changes

---
+++ SmartBot.Tests/Dialogs/MainDialogTests.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Testing;
+using Microsoft.Bot.Builder.Testing.XUnit;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Newtonsoft.Json;
+using SmartBot.Dialogs;
+using SmartBot.Tests.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace SmartBot.Tests.Dialogs
+{
+    public class MainDialogTests : BotTestBase
+    {        
+        private readonly Mock<ILogger<MainDialog>> _mockLogger;
+
+        public MainDialogTests(ITestOutputHelper output)
+            : base(output)
+        {
+            _mockLogger = new Mock<ILogger<MainDialog>>();
+         }
+
+        /// <summary>
+        /// Loads the embedded json resource with the LUIS as a string.
+        /// </summary>
+        private string GetEmbeddedTestData(string resourceName)
+        {
+            using (var stream = GetType().Assembly.GetManifestResourceStream(resourceName))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
---
+++ SmartBot/Dialogs/MainDialog.cs
@@ -0,0 +1,91 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio CoreBot v4.16.0
+
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Choices;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+using Microsoft.Recognizers.Text.DataTypes.TimexExpression;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SmartBot.Dialogs
+{
+    public class MainDialog : ComponentDialog
+    {
+        private readonly ILogger _logger;
+        
+        // Dependency injection uses this constructor to instantiate MainDialog
+        public MainDialog(ILogger<MainDialog> logger)
+            : base(nameof(MainDialog))
+        {
+            _logger = logger;
+
+            AddDialog(new TextPrompt(nameof(TextPrompt)));
+            AddDialog(new ChoicePrompt(nameof(ChoicePrompt)));
+            
+            var waterfallSteps = new WaterfallStep[]
+            {
+                    ConfirmAzureLevelAsync,
+                    ConfirmLearningOptionsAsync,
+                    AzureLearningOptionAsyn,
+                    FinalStepAsync
+            };
+
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), waterfallSteps));
+
+            // The initial child Dialog to run.
+            InitialDialogId = nameof(WaterfallDialog);
+        }
+
+        private async Task<DialogTurnResult> ConfirmAzureLevelAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var azureList = new PromptOptions
+            {
+                Choices=new List<Choice> { new Choice("Beginner"), new Choice("Intermediate") , new Choice("Expert") },
+                Prompt=MessageFactory.Text("What is your expertize level in azure techonology")
+            };
+
+            return await stepContext.PromptAsync(nameof(ChoicePrompt),azureList, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> ConfirmLearningOptionsAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            var azureLearnList = new PromptOptions
+            {
+                Choices = new List<Choice> { new Choice("Read Up"), new Choice("Certification"), new Choice("Practice Test") },
+                Prompt = MessageFactory.Text("What do like to do?")
+            };
+
+            return await stepContext.PromptAsync(nameof(ChoicePrompt), azureLearnList, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> AzureLearningOptionAsyn(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {  
+            SmartBot_Content.ModelInput content = new SmartBot_Content.ModelInput()
+            {
+                Col0 = stepContext.Context.Activity.Text
+            };
+
+            var predict = SmartBot_Content.Predict(content);
+
+            var promptMessage = MessageFactory.Text(predict.PredictedLabel);
+
+            return await stepContext.PromptAsync(nameof(TextPrompt), new PromptOptions { Prompt = promptMessage }, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> FinalStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            //Restart the main dialog
+            var promptMessage = "what other information required on Azure?";
+            return await stepContext.ReplaceDialogAsync(InitialDialogId, promptMessage,cancellationToken);
+        }
+
+    }
+}
