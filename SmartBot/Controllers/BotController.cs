22db352805480e298ecfd89fa3af658dd3b767de
Kiran Gowda <kiran.kj4@outlook.com>
2022-10-05 19:16:38 -0500

    removed unwantned code

--- SmartBot/Controllers/BotController.cs
+++ SmartBot/Controllers/BotController.cs
@@ -1,9 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-//
-// Generated with Bot Builder V4 SDK Template for Visual Studio CoreBot v4.16.0
-
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using System.Threading.Tasks;
